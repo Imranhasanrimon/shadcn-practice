0e03a516fd98204f87eeac10970f3adbec86ebbc
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-03-17 12:57:33 +0600

    making awesome card by several components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Loader2 } from 'lucide-react'
 import './App.css'
+import { Button } from './components/ui/button'
+import { Calendar } from './components/ui/calendar'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { CardWithForm } from './tools/CardWithForm'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+      <Button className=" mt-2">
+        <Loader2 className="animate-spin" />
+        Please wait
+      </Button>
+
+      <Calendar
+        mode="single"
+
+        // selected={date}
+        // onSelect={setDate}
+        className="rounded-md border"
+      />
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+
+      <CardWithForm ></CardWithForm>
+
+
     </>
   )
 }
